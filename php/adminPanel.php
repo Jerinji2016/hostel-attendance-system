10c0247b68c8e3bead16faf2ab7dfd983debb328
Jerinji2016 <jerinji2016@gmail.com>
2019-06-10 22:51:09 +0530

    changes

--- php/adminPanel.php
+++ php/adminPanel.php
@@ -14,7 +14,6 @@
 		<?php include 'sidebarAdmin.php' ?>
 
 		<div id="main">
-			
 		</div>
 	</body>
 </html>
--- php/attendanceTab_admin.php
+++ php/attendanceTab_admin.php
@@ -33,7 +33,6 @@
 		<input type="text" value="2" id="display" style="display: none">
 		<?php include 'sidebar.php' ?>
 		<script type="text/javascript"> focusSidebar(); </script>
-		
 		<div id="main" style="width: 1000px; margin: 0px auto; margin-top: 130px; padding: 0%">
 			<div style="width: 400px; margin: 0px auto">
 				<div style="border-radius: 25px 25px 0px 0px; padding: 15px 20px; background-color: #0C0C0C; 
