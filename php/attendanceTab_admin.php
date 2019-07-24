ef172458d7ff94d7426ab385eb58f5851be01d6a
Divya-99 <divyamalex@gmail.com>
2019-07-24 13:32:42 +0530

    nothing

--- php/attendanceTab_admin.php
+++ php/attendanceTab_admin.php
@@ -9,7 +9,7 @@ include 'session.php';
 			View Attendance Details
 		</title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="../css/login.css">
 		<link rel="stylesheet" type="text/css" href="../css/autocomplete.css">
 		<link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
